c8569323333468410a41d7b7fb82a3d248b9028d
mstrader <mstrader@physics.ucsb.edu>
2012-11-19 12:54:23 -0800

    Cleaned up and added minimum documentation

--- flatcal/flatCal.py
+++ flatcal/flatCal.py
@@ -1,9 +1,12 @@
 #!/bin/python
-#Opens a twilight flat h5 and makes the spectrum of each pixel.
-#Then takes the median of each energy over all pixels
-#A factor is then calculated for each energy in each pixel of its
-#twilight count rate / median count rate
-#This is added to the flat factor in the photonList
+"""
+Author: Matt Strader        Date:August 19,2012
+Opens a twilight flat h5 and makes the spectrum of each pixel.
+Then takes the median of each energy over all pixels
+A factor is then calculated for each energy in each pixel of its
+twilight count rate / median count rate
+The factors are written out in an h5 file
+"""
 
 import sys,os
 import tables
@@ -13,20 +16,19 @@ from util.ObsFile import ObsFile
 
 class FlatCal:
     def __init__(self,flatFileName=None,wvlCalFileName=None,flatCalFileName=None):
+        """
+        opens flat file,sets wavelength binnning parameters, and calculates flat factors for the file
+        """
         self.wvlBinWidth = 100 #angstroms
         self.wvlStart = 3000 #angstroms
         self.wvlStop = 13000 #angstroms
         self.nWvlBins = int((self.wvlStop - self.wvlStart)/self.wvlBinWidth)
         
-
-        if flatFileName == None:
-            self.loadFactors()
-        else:
-            self.loadFlatFile(flatFileName)
-            self.flatFile.loadWvlCalFile(wvlCalFileName)
-            self.loadFlatSpectra()
-            self.calculateFactors()
-            self.writeFactors(flatCalFileName)
+        self.loadFlatFile(flatFileName)
+        self.flatFile.loadWvlCalFile(wvlCalFileName)#load the wavelength cal to be applied to the flat
+        self.loadFlatSpectra()
+        self.calculateFactors()
+        self.writeFactors(flatCalFileName)
 
     def __del__(self):
         try:
@@ -42,7 +44,6 @@ class FlatCal:
         self.nCol = self.flatFile.nCol
         
     def loadFlatSpectra(self):
-        scratchDir = os.getenv('INTERM_PATH','.')
         flatDir = os.path.join(scratchDir,'flatCalSolnFiles')
         self.spectra = [[[] for i in xrange(self.nCol)] for j in xrange(self.nRow)]
         for iRow in xrange(self.nRow):
@@ -50,11 +51,8 @@ class FlatCal:
                 print iRow,iCol,
                 count = self.flatFile.getPixelCount(iRow,iCol)
                 print count
-                #wvlList = self.flatFile.getPixelWvlList(iRow,iCol)
-                #self.spectra[iRow][iCol],self.wvlBinEdges = np.histogram(wvlList,bins=self.nWvlBins,range=(self.wvlStart,self.wvlStop))
                 self.spectra[iRow][iCol],self.wvlBinEdges = self.flatFile.getPixelSpectrum(iRow,iCol,wvlStart=self.wvlStart,wvlStop=self.wvlStop,wvlBinWidth=self.wvlBinWidth,weighted=False,firstSec=0,integrationTime=-1)
         self.spectra = np.array(self.spectra)
-        np.save(os.path.join(flatDir,'flatSpectra.npy'),self.spectra)
         self.calculateMedians()
         return self.spectra
 
@@ -65,21 +63,26 @@ class FlatCal:
         for iWvl in xrange(self.nWvlBins):
             print iWvl,self.wvlBinEdges[iWvl],
             spectrum = spectra2d[:,iWvl]
-            goodSpectrum = spectrum[spectrum != 0]
+            goodSpectrum = spectrum[spectrum != 0]#dead pixels need to be taken out before calculating medians
             self.wvlMedians[iWvl] = np.median(goodSpectrum)
             print self.wvlMedians[iWvl]
             
         return self.wvlMedians
         
     def calculateFactors(self):
-        fudgedSpectra = self.spectra
-        #fudgedSpectra[fudgedSpectra == 0] =1.0
+        """
+        finds flat cal factors as medians/pixelSpectra for each pixel
+        """
         self.flatFactors = np.divide(self.wvlMedians,self.spectra)
+        #set factors that will cause trouble to 1
         self.flatFactors[self.flatFactors == np.inf]=1.0
         self.flatFactors[self.flatFactors == 0]=1.0
         np.save('/ScienceData/intermediate/factors.npy',self.flatFactors)
 
     def writeFactors(self,flatCalFileName):
+        """
+        Writes an h5 file to put calculated flat cal factors in
+        """
         scratchDir = os.getenv('INTERM_PATH')
         flatDir = os.path.join(scratchDir,'flatCalSolnFiles')
         fullFlatCalFileName = os.path.join(flatDir,flatCalFileName)
