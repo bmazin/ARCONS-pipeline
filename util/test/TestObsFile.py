f7229b27b1b6dbf7764092289f63201fea46fda0
Chris Stoughton <chris.stoughton@gmail.com>
2012-12-12 15:20:58 -0800

    Detect and mask out flashes.  FileName.py deals with file names

---
+++ util/test/TestObsFile.py
@@ -0,0 +1,69 @@
+import os
+import unittest
+import numpy as np
+from util import ObsFile, FileName
+from interval import interval, inf, imath
+import matplotlib.pyplot as plt
+import inspect
+
+class TestObsFile(unittest.TestCase):
+    def testCalculateSlicesMiddle(self):
+        "one interval in the middle of the set of timestamps"
+        secs = np.arange(10)
+        inter = interval[2,6]
+        slices = ObsFile.calculateSlices(inter, secs)
+        self.assertEquals(slices, ["0:2","7:10"])
+        new = ObsFile.repackArray(secs, slices)
+        print "new=",new
+        self.assertTrue(npEquals(new,np.array([0,1,7,8,9])))
+
+    def testCalculateSlicesBeginningAndEnd(self):
+        "one interval at the beginning and one at the end"
+        secs = np.arange(10)
+        inter = interval[0,3] | interval[8,9]
+        slices = ObsFile.calculateSlices(inter, secs)
+        new = ObsFile.repackArray(secs, slices)
+        print "new=",new
+        self.assertTrue(npEquals(new, np.array([4,5,6,7])))
+
+    def testParsePhotonPacketsWithIntervalMask(self):
+        """
+        parse one second of data with and without masking a range of times.
+        plot the result
+        """
+        fn = FileName.FileName('LICK2012','20120919',  '20120920-092626')
+        fullFileName = fn.obs()
+        obsFile = ObsFile.ObsFile(fn.obs())
+        pixel = obsFile.getPixel(3,4)
+        packet = pixel[123]
+        ts,pfp,bl = obsFile.parsePhotonPackets(packet)
+        # specific to the file, pixel, and second we parsed
+        self.assertEquals(len(ts),66)
+        xs = [200000,400000]
+        inter = interval[xs[0],xs[1]]
+        tsm,pfpm,blm = obsFile.parsePhotonPackets(packet, inter)
+        # specific to the file, pixel, and second we parsed
+        self.assertEquals(len(tsm),50)
+        plt.plot(ts,pfp,label="all points");
+        plt.plot(tsm,pfpm,'ro',label="not masked points");
+        ys = plt.axis()
+        plt.plot([xs[0],xs[0]],[ys[2],ys[3]],'green')
+        plt.plot([xs[1],xs[1]],[ys[2],ys[3]],'green')
+        plt.legend(loc="lower right")
+        plt.xlabel("Time (ticks)")
+        plt.ylabel("peak flux parabola fit")
+        plt.title("mask times from %d to %d ticks" % (xs[0],xs[1]))
+        plt.savefig(fn.makeName(inspect.stack()[0][3]+"_",""))
+
+def npEquals(a,b):
+    if (len(a) != len(b)):
+        return False
+
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            return False
+
+    return True
+
+if __name__ == '__main__':
+    unittest.main()
