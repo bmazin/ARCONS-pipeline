1d32999172102df9ea64ad62df53828e9718fd95
Chris Stoughton <chris.stoughton@gmail.com>
2013-01-25 14:34:35 -0800

    speed up histogramming

---
+++ util/test/TestHg.py
@@ -0,0 +1,70 @@
+import unittest
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from util import hgPlot
+import inspect
+class TestHg(unittest.TestCase):
+    """
+    Test options for filling and plotting histograms
+    """
+    def testCompare(self):
+        """
+        Demonstrate that np.bincounts is 40 times faster than np.histogram
+        """
+        nPixels = 100
+        values = []
+        for i in range(nPixels):
+            values.append(np.random.random_integers(0,1000000, 100))
+            values[i].sort()
+
+        # measure time for np.histogram
+        begin = time.time()        
+        for i in range(nPixels):
+            hg = np.histogram(values[i], 1000000, range=(0,1000000)) # 3.8 sec
+        end = time.time()
+        delta = end - begin
+        print "np.histogram elapsed time is",delta
+
+        # measure time for np.bincount
+        begin = time.time()        
+        for i in range(nPixels):
+            hg = np.bincount(values[i],minlength=1000000) # 0.097 sec/100
+        end = time.time()
+        delta = end - begin
+        print "np.bincount elapsed time is",delta
+
+    def testHgPlot1(self):
+        xmax = 5
+        hg = np.histogram([0,1,1,1,1,1,2,2],bins=xmax, range=(-0.5,xmax-0.5))
+        x,y = hgPlot.getPlotValues(hg, ylog=False)
+        plt.clf()
+        plt.plot(x,y)
+        plt.margins(0.1, 0.1)
+        tfn = inspect.stack()[0][3]
+        plt.savefig(tfn)
+
+    def testHgPlot2(self):
+        hg = [0,0,1,8,4,0]
+        x,y = hgPlot.getPlotValues(hg, ylog=False)
+        plt.clf()
+        plt.plot(x,y)
+        plt.margins(0.1, 0.1)
+        tfn = inspect.stack()[0][3]
+        plt.savefig(tfn)
+
+    def testHgPlot3(self):
+        hg = [10000,0.1,0,1]
+        x,y = hgPlot.getPlotValues(hg, ylog=False)
+        for i in range(len(x)):
+            print i, x[i],y[i]
+        plt.clf()
+        plt.plot(x,y)
+        plt.margins(0.1, 0.1)
+        plt.yscale('log')
+        tfn = inspect.stack()[0][3]
+        plt.savefig(tfn)
+
+
+if __name__ == '__main__':
+    unittest.main()
