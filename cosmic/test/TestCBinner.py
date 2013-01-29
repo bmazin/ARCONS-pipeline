bcddfe4e3ae8e8f9db4d35f313405a8001196853
Chris Stoughton <chris.stoughton@gmail.com>
2013-01-29 13:07:46 -0800

    binning in cython

--- cosmic/test/TestCBinner.py
+++ cosmic/test/TestCBinner.py
@@ -2,12 +2,19 @@ import os
 import unittest
 import numpy as np
 from cosmic import c_binner
+from cosmic import tsBinner
 class TestCBinner(unittest.TestCase):
 
-    def testInterface(self):
+    def testCBinner(self):
         
         ts = np.array([1,3,5,6], dtype=np.uint64)
         bins = np.zeros(10, dtype=np.int64)
+        c_binner.binner(ts,bins)
+        print "bins=",bins
+
+    def testTsBinner(self):
+        ts = np.array([1,3,5,6], dtype=np.uint64)
+        bins = np.zeros(10, dtype=np.int)
         c_binner.binner(ts,bins)
         print "bins=",bins
 
