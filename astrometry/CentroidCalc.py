4f2caf768f663c108e1247a97a0d398bc66a6686
PSzypryt <pszypryt@physics.ucsb.edu>
2013-01-31 17:13:58 -0800

    Fixed up centroiding code, now displays a plot and lets you select a guess by clicking a pixel. Also allows for variable integration time.

--- astrometry/CentroidCalc.py
+++ astrometry/CentroidCalc.py
@@ -1,58 +1,64 @@
 import numpy as np
 from tables import *
-import ConfigParser
+import sys
 import ephem
 import PyGuide as pg
 import math
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-# Initial guess of centroid location
-xguess = 14.0
-yguess = 14.0
-xyguess = [yguess,xguess]
+import os
+from PyQt4.QtGui import *
+
+class MouseMonitor():
+    def __init__(self):
+        pass
+
+    def on_click(self,event):
+	self.xyguess = [event.xdata,event.ydata]
+   
+    def connect(self):
+        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+	
 
 # Set the path and h5 file names to be imported
-path = '/home/pszypryt/Scratchwork/'
-h5beamfile = 'beamimage_sci3gamma.h5'
-h5obsfile = 'obs_20120904-230531.h5'
-n = h5obsfile.find('.')
-centroid_list_identifier = h5obsfile[0:n]
+h5beamfile = '/ScienceData/sci4alpha/sci4_beammap_palomar.h5'
+
+app = QApplication(sys.argv)
+startingSaveDir = os.getcwd()
+savePath = QFileDialog.getExistingDirectory(None,'Choose Save Path',startingSaveDir, QFileDialog.ShowDirsOnly)
+startingh5Dir = '/ScienceData/PAL2012/'
+h5obsfile =str(QFileDialog.getOpenFileName(None, 'Choose Observation File',startingh5Dir, filter=str("H5 (*.h5)")))
+n0 = h5obsfile.rfind('/')
+n1 = h5obsfile.find('.')
+centroid_list_identifier = h5obsfile[n0+1:n1]
 txtout = 'centroids_' + centroid_list_identifier + '.txt'
-ini_file = path + 'Lick2012Initialization.ini'
 
-# Config File Information
-Config = ConfigParser.ConfigParser()
-Config.read(ini_file)
-grid_width = Config.getint('ARRAY','GRID_WIDTH')
-
-grid_height = Config.getint('ARRAY','GRID_HEIGHT')
+grid_width = 44
+grid_height = 46
 
 # Make map of where pixels fit in grid in format '/r0/p123/'
-h5beam = openFile(path + h5beamfile, mode = 'r')
+h5beam = openFile(h5beamfile, mode = 'r')
 location_strings = h5beam.root._f_getChild('/beammap/beamimage')
 
 # Open the obs file and extract photon data to calculate com positions in time frames
-h5obs = openFile(path + h5obsfile, mode = 'r')
+h5obs = openFile(h5obsfile, mode = 'r')
 exptime = h5obs.root.header.header.col('exptime')[0]
+
 try:
     ts = h5obs.root.header.header.col('unixtime')[0]
 except KeyError:
     print 'Using "ut" instead of "unixtime" in header'
-    ts = h5obs.root.header.header.col('ut')[0]    
+    ts = h5obs.root.header.header.col('ut')[0]
+
 flux_cube = np.zeros(((grid_height,grid_width,exptime)))
-sum_x = np.zeros(exptime)
-sum_y = np.zeros(exptime)
-mass = np.zeros(exptime)
-com_x = np.zeros(exptime)
-com_y = np.zeros(exptime)
 for y in range(grid_height):
     for x in range(grid_width):
         pn = location_strings[y][x] + 't' + str(int(ts))
         data = h5obs.root._f_getChild(pn).read()
         for t in range(exptime):
             flux_cube[y][x][t] = len(data[t])
-            
+
 # Specify pixel info CCDInfo(bias,readNoise,ccdGain,satLevel)
 ccd = pg.CCDInfo(0,0.00001,1,2500)
 
@@ -62,16 +68,37 @@ mask = np.zeros((grid_height,grid_width))
 # Create a mask for saturated pixels, 0 for okay, 1 for saturated
 satMask = np.zeros((grid_height,grid_width))
 
-f = open(path + txtout,'w')
-for t in range(exptime):
-    grid_to_centroid = np.zeros((grid_height,grid_width))
+integration_time= 10
+xyguess=[0,0]
+
+f = open(savePath+ '/' + txtout,'w')
+for t in range(int(exptime/integration_time)):
+    map = MouseMonitor()
+    map.fig = plt.figure()
+    pltmat = np.zeros((grid_height,grid_width))
     for y in range(grid_height):
         for x in range(grid_width):
-            grid_to_centroid[y][x] = flux_cube[y][x][t]          
-    pyguide_output = pg.centroid(grid_to_centroid,mask,mask,xyguess,10,ccd,0,False)
-    xycenter = pyguide_output.xyCtr
-    f.write(str(xycenter[0]) + '\t' + str(xycenter[1]) + '\n')
-    if (t==20):
-        plt.imshow(grid_to_centroid,cmap = plt.cm.gray)
-f.close()
-plt.show()
+	    for i in range(integration_time):
+                pltmat[y][x]+=flux_cube[y][x][int(t*integration_time+i)]/integration_time
+    map.ax = map.fig.add_subplot(111)
+    map.ax.matshow(pltmat,cmap = plt.cm.gray, origin = 'lower')
+    map.connect()
+    plt.show()
+    try:
+    	xyguess = map.xyguess
+    except AttributeError:
+	pass
+    print 'Guess = ' + str(xyguess)
+    pyguide_output = pg.centroid(pltmat,mask,mask,xyguess,10,ccd,0,False)
+    try:
+        xycenter = pyguide_output.xyCtr
+        print 'Calculated = ' + str(xycenter)
+        f=open(savePath+ '/' + txtout,'a')
+        f.write(str(xycenter[0]) + '\t' + str(xycenter[1]) + '\n')
+        f.close()
+    except TypeError:
+        print 'Cannot centroid, using guess'
+        f=open(savePath+ '/' + txtout,'a')
+        f.write(str(xyguess[0]) + '\t' + str(xyguess[1]) + '\n')
+        f.close()
+
