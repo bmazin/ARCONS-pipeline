3e51444d742c555f69631b2e0c7143ec6d2d96c1
Julian van Eyken <vaneyken@physics.ucsb.edu>
2013-02-05 20:10:52 -0800

    Update to Arp147 tiling quick-fudge code.

--- examples/Pal2012_Arp147/Arp147ImageTile.py
+++ examples/Pal2012_Arp147/Arp147ImageTile.py
@@ -6,14 +6,20 @@ JvE Jan 30 2013
 import numpy as np
 import datetime as dt
 import util.ObsFile as ObsFile
+import util.utils as utils
 import os.path
 import hotpix.hotPixels as hotPixels
+import matplotlib.pylab as mpl
+import pyfits
+import pickle
 
 def makeArp147Image(obsFileList='inputFiles.list',
-                    offsetLog='raster_20121209-052620-060135-edited.log', 
+                    offsetLog='raster_20121209-052620-060135-edited.log',
                     wvlCalFileName='calsol_20121209-060704.h5',
                     flatCalFileName='flatsol_20121210.h5',
-                    workingDir='/Users/vaneyken/Data/Palomar2012/arp147/Arp147WorkingDir/'):
+                    workingDir='/Users/vaneyken/Data/Palomar2012/arp147/Arp147WorkingDir/',
+                    showMasks=False,
+                    outputFile=''):
     
     plateScale = 0.336281230787  #0.402      #in arcsec per pix.
     imAngularWidth = 60       #Virtual image width in arcsec 
@@ -22,9 +28,15 @@ def makeArp147Image(obsFileList='inputFiles.list',
                           #(assume constant...!)
     slewTime = 2          #Block out this much time after each slew start (sec).
     
-    wavebands = [{'color':'blue',   'min':4000,  'max':5000}, #Define names and wavelength ranges for the 
-                 {'color':'green', 'min':4500,  'max':6500}, #wavebands. (Wavelength in Anstroms)
-                 {'color':'red',  'min':5000,  'max':8000}]
+    wavebands = [{'color':'red', 'min':6000, 'max':8000}, #Define names and ranges (Ang) 
+                 {'color':'green', 'min':5000, 'max':6000}, #for the wavebands.
+                 {'color':'blue', 'min':4000, 'max':5000}, #NOTE - REDDEST BAND MUST GO FIRST.
+                ]
+    
+    #wavebands = [{'color':'clear',   'min':4000,  'max':8000}] #Define names and ranges (Ang) 
+
+    ##Find the index of the reddest band in 'wavebands'
+    #reddestBand = np.argmax([(x['min']+x['max'])/2 for x in wavebands])
     
     raOffset = np.array([])       #To take a list of RA offsets
     decOffset = np.array([])      #Ditto for dec.
@@ -37,15 +49,16 @@ def makeArp147Image(obsFileList='inputFiles.list',
     decUnitVec = -np.array([-np.sin(theta), np.cos(theta)]) / plateScale
     
     #Read in file name list
-    try:
-        f = open(os.path.join(workingDir,obsFileList), 'r')
-        obsFileNames = np.array([os.path.join(workingDir,line.strip()) for line in f.readlines()])
-    finally:
-        f.close()
+    #try:
+    f = open(os.path.join(workingDir, obsFileList), 'r')
+    obsFileNames = np.array([os.path.join(workingDir, line.strip()) for line in f.readlines()])
+    #finally:
+    #    print 'Problem reading file list'
+    #    f.close()
     
     #Read in offset log
     try:
-        f = open(os.path.join(workingDir,offsetLog), 'r')
+        f = open(os.path.join(workingDir, offsetLog), 'r')
         for eachLine in f:
             splitLine = eachLine.split()
             if len(splitLine) >= 3:             #If <3 entries in line, just skip it. (Takes care of empty lines).
@@ -76,8 +89,8 @@ def makeArp147Image(obsFileList='inputFiles.list',
                          '%a, %d %b %Y %H:%M:%S')
         obsLength = dt.timedelta(seconds=float(obsFile.getFromHeader('exptime')))
         tEnd = tStart + obsLength
-        obsStart = np.append(obsStart,tStart)
-        obsEnd = np.append(obsEnd,tEnd)
+        obsStart = np.append(obsStart, tStart)
+        obsEnd = np.append(obsEnd, tEnd)
         print eachFileName, tStart, tEnd
 
     nDetRow = obsFile.nRow  #Number of rows and columns on the detector
@@ -90,15 +103,15 @@ def makeArp147Image(obsFileList='inputFiles.list',
     nVGridCol = np.ceil(imAngularWidth / plateScale)
     
     #Location of top left corner of a detector image if it were centered on the virtual grid.
-    xCornerCen = np.floor(nVGridCol/2 - nDetCol/2)
-    yCornerCen = np.floor(nVGridRow/2 - nDetRow/2)
+    xCornerCen = np.floor(nVGridCol / 2 - nDetCol / 2)
+    yCornerCen = np.floor(nVGridRow / 2 - nDetRow / 2)
 
     #Initialise 'images' dictionary to look like:
     #{'red':A, 'green':B, 'blue':C} (or whatever)
     #Where A, B, C (,...) are 3D arrays (npointings x representing an image for each pointing. 
     images = {}
     for eachWaveband in wavebands:
-        images[eachWaveband['color']] = np.zeros((nLoc,nVGridRow,nVGridCol))
+        images[eachWaveband['color']] = np.zeros((nLoc, nVGridRow, nVGridCol))
         images[eachWaveband['color']].fill(np.NaN)
     
     #assert 1==0
@@ -121,36 +134,58 @@ def makeArp147Image(obsFileList='inputFiles.list',
             print eachWaveband['color']
             #Stack all the images from all obs files who's times overlap this pointing
             im = np.zeros((nDetRow, nDetCol))    #Initialise an empty image
-            for eachFileName,eachObsStart,eachObsEnd in zip(obsNamesAtLoc,obsStartAtLoc,obsEndAtLoc):
+            totIntTime = 0                       #To take summed integration time (sec) for this pointing.
+            for eachFileName, eachObsStart, eachObsEnd in zip(obsNamesAtLoc, obsStartAtLoc, obsEndAtLoc):
                 print eachFileName
                 obsFile = ObsFile.ObsFile(eachFileName)
-                obsFile.loadFlatCalFile(os.path.join(workingDir,flatCalFileName))
-                obsFile.loadWvlCalFile(os.path.join(workingDir,wvlCalFileName))
+                obsFile.loadFlatCalFile(os.path.join(workingDir, flatCalFileName))
+                obsFile.loadWvlCalFile(os.path.join(workingDir, wvlCalFileName))
                 obsFile.setWvlCutoffs(eachWaveband['min'], eachWaveband['max'])
                 #Find integration start/end times in seconds relative to start of obs. file
                 #Note that negative datetime objects give unexpected results - so don't go there!
                 if eachLocStart < eachObsStart:
                     firstSec = 0
                 else:
-                    firstSec = (eachLocStart-eachObsStart).seconds      #No. of seconds from start time of obs file.
+                    firstSec = (eachLocStart - eachObsStart).seconds      #No. of seconds from start time of obs file.
                 if eachLocEnd > eachObsEnd:
-                    lastSec = (eachObsEnd-eachObsStart).seconds        #Will integrate to the end of the obs. file.
+                    lastSec = (eachObsEnd - eachObsStart).seconds        #Will integrate to the end of the obs. file.
                 else:
-                    lastSec = (eachLocEnd-eachObsStart).seconds
-                integrationTime = lastSec-firstSec
+                    lastSec = (eachLocEnd - eachObsStart).seconds
+                integrationTime = lastSec - firstSec
                 im += obsFile.getPixelCountImage(firstSec=firstSec,
                                            integrationTime=integrationTime,
                                            weighted=True)
+                totIntTime+=integrationTime     #In seconds
                 #assert 1==0
                 del(obsFile)
             
-            ########
-            im[im==0] = np.nan      #Set any dead pixels to NaN...
-            hpMask = hotPixels.checkInterval(image=im)['mask']
-            im[hpMask] = np.nan     #Same for any hot pixels
-            #Deal with any hot pixel masking etc. here (im = maskedVersionOf(im)....)
-            ########
-            
+            #Mask any bad pixels
+            if eachWaveband['color'] == wavebands[0]['color']:       #If we're on the reddest band
+                hpMask = hotPixels.checkInterval(image=im, fwhm=4.0)['mask']  #make a pixel mask.
+                hpMask = (hpMask > 0)   #Turn into a boolean array.
+
+            deadMask = (im == 0)                #Dead pixels
+            combinedMask = hpMask | deadMask    #Always uses the hot pixel mask from the reddest (first) band.
+
+            if showMasks:
+                imCopy = np.array(im)      #Make a copy, because otherwise changes to im apparently can show up in matshow even AFTER the matshow call!!
+                mpl.matshow(imCopy, vmax=np.percentile(imCopy[~np.isnan(imCopy)], 99.5))
+                mpl.colorbar()
+                x = np.arange(np.shape(im)[1])
+                y = np.arange(np.shape(im)[0])
+                xx, yy = np.meshgrid(x, y)
+                mpl.scatter(xx[hpMask], yy[hpMask], c='y')
+                mpl.title('#' + str(iLoc) + ', ' + str(eachLocStart) + ' to ' + str(eachLocEnd)
+                         + 's, ' + str(eachWaveband['color']))
+                
+            im[combinedMask] = np.nan     #Set any bad pixels to NaN.
+       
+            #assert 1==0
+            #Scale the image for the integration time:
+            print 'Total integration time: '+str(totIntTime)
+            im /= totIntTime
+       
+       
             #Determine where image should be located on the virtual grid
             gridOffset = eachRaOffset * raUnitVec + eachDecOffset * decUnitVec
             xCorner = np.round(xCornerCen + gridOffset[0])
@@ -159,7 +194,7 @@ def makeArp147Image(obsFileList='inputFiles.list',
             
             #And put it in the image stack, in the right location. All values in
             #virtual grid outside the current image will remain NaN for this pointing.
-            images[eachWaveband['color']][iLoc,yCorner:yCorner+nDetRow, xCorner:xCorner+nDetCol] = im
+            images[eachWaveband['color']][iLoc, yCorner:yCorner + nDetRow, xCorner:xCorner + nDetCol] = im
             
             #assert 1==0
     #Now have a stack of images in 'images'
@@ -179,13 +214,58 @@ def makeArp147Image(obsFileList='inputFiles.list',
     print 'Done....'
     #assert 1==0
     
-    return {'final':finalImage, 'stack':images}
+    result = {'final':finalImage, 'stack':images}
+    
+    if type(outputFile) is str and len(outputFile) > 0:
+        try:
+            f = open(os.path.join(workingDir, outputFile), 'wb')
+            pickle.dump(result, f)
+        except OSError:
+            print 'Could not write results to output file.' 
+        finally:
+            f.close()
+        
+    return result
 
     
+
+
+
+def makeRGBimage(result, fitsBaseName='Arp147-',
+                  workingDir='/Users/vaneyken/Data/Palomar2012/arp147/Arp147WorkingDir/',
+                  interpolate=False, medianSmooth=False):
+    '''
+    Take a 'result' (as returned by makeArp147Image()) and make an RGB image out
+    of it. Also create fits files of the three components.
+
+    INPUTS:
+        result: as returned by makeArp147Image()
+        fitsBaseName: fits files are output as fistBaseName+[color]+'.fits'
+    
+    '''
+    
+    rgb = result['final']       #Convenient shorthand
+    
+    #Write to fits files
+    for color in rgb.keys():
+        fileName = fitsBaseName + color + '.fits'
+        component = rgb[color]
+        if interpolate:
+            component[component == 0] = np.nan
+            component = utils.replaceNaN(component, iterate=False)
+        if medianSmooth:
+            component = utils.median_filterNaN(component, size=3)
+            
+        hdu = pyfits.PrimaryHDU(component)
+        hdu.writeto(os.path.join(workingDir, fileName))
+        
+
+
 
 
 
 if __name__ == "__main__":
     
-    result = makeArp147Image()
+    out1 = makeArp147Image(showMasks=True,
+                               offsetLog='raster_20121209-052620-060135-edited-short.log')
     
