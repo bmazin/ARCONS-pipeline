fc28d24f8a3f7c134f0997ad7985f2014cc4c789
mstrader <mstrader@physics.ucsb.edu>
2013-03-21 14:40:10 -0700

    popup now has a function to pop up an array image

--- util/popup.py
+++ util/popup.py
@@ -4,29 +4,34 @@ from PyQt4 import QtGui
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+from multiprocessing import Process
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 from matplotlib.figure import Figure
+import matplotlib
+from functools import partial
 
 class PopUp(QMainWindow):
-    def __init__(self, parent=None,plotFunc=None,clickFunc=None,title=''):
+    def __init__(self, parent=None,plotFunc=None,title='',separateProcess=False):
+        if separateProcess:
+            proc = Process(target=PopUp,kwargs={'plotFunc':plotFunc,'title':title})
+            proc.start()
+            return None
         if parent==None:
             app = QApplication(sys.argv)
         QMainWindow.__init__(self, parent)
         self.setWindowTitle(title)
         self.plotFunc = plotFunc
-        self.clickFunc = clickFunc
         self.create_main_frame(title)
         self.create_status_bar()
         if plotFunc != None:
-            plotFunc(fig=self.fig,axes=self.axes0)
+            plotFunc(fig=self.fig,axes=self.axes)
         self.show()
         if parent==None:
             app.exec_()
 
-    def clickCanvas(self,event):
-        self.clickFunc(fig=self.fig,axes=self.axes0,event=event)
-        self.canvas.draw()
+    def draw(self):
+        self.fig.canvas.draw()
 
     def create_main_frame(self,title):
         self.main_frame = QWidget()
@@ -35,10 +40,8 @@ class PopUp(QMainWindow):
         self.fig = Figure((5, 5), dpi=self.dpi)
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self.main_frame)
-        self.axes0 = self.fig.add_subplot(111)
-        self.axes0.set_title(title)
-        if self.clickFunc != None:
-            cid=self.canvas.mpl_connect('button_press_event', self.clickCanvas)
+        self.axes = self.fig.add_subplot(111)
+        #self.axes.set_title(title)
 
         # Create the navigation toolbar, tied to the canvas
         self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
@@ -51,6 +54,36 @@ class PopUp(QMainWindow):
     def create_status_bar(self):
         self.status_text = QLabel("")
         self.statusBar().addWidget(self.status_text, 1)
+
+
+    def plotArray(self,image,normNSigma=3,title=''):
+        handleMatshow = self.axes.matshow(image,cmap=matplotlib.cm.gnuplot2,origin='lower',vmax=np.mean(image)+normNSigma*np.std(image))
+        self.fig.cbar = self.fig.colorbar(handleMatshow)
+        cid = self.fig.canvas.mpl_connect('scroll_event', partial(onscroll_cbar, self.fig))
+        cid = self.fig.canvas.mpl_connect('button_press_event', partial(onclick_cbar, self.fig))
+        self.draw()
+
+def onscroll_cbar(fig, event):
+    if event.inaxes is fig.cbar.ax:
+        increment=0.05
+        currentClim = fig.cbar.mappable.get_clim()
+        if event.button == 'up':
+            newClim = (currentClim[0],(1.+increment)*currentClim[1])
+        if event.button == 'down':
+            newClim = (currentClim[0],(1.-increment)*currentClim[1])
+        fig.cbar.mappable.set_clim(newClim)
+        fig.canvas.draw()
+
+def onclick_cbar(fig,event):
+    if event.inaxes is fig.cbar.ax:
+        if event.button == 1:
+            fig.oldClim = fig.cbar.mappable.get_clim()
+            fig.cbar.mappable.set_clim(fig.oldClim[0],event.ydata*fig.oldClim[1])
+            fig.canvas.draw()
+        if event.button == 3:
+            fig.oldClim = fig.cbar.mappable.get_clim()
+            fig.cbar.mappable.set_clim(fig.oldClim[0],1/event.ydata*fig.oldClim[1])
+            fig.canvas.draw()
 
 def main():
     app = QApplication(sys.argv)
