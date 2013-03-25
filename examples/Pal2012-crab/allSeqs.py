3e877657fcffbeafe1ecaf20245d4d4e68d1cf25
mstrader <mstrader@physics.ucsb.edu>
2013-03-25 10:00:48 -0700

    Added a sequence from another day to be folded in allSeqs.py

--- examples/Pal2012-crab/allSeqs.py
+++ examples/Pal2012-crab/allSeqs.py
@@ -53,10 +53,15 @@ def calculateFreq(mjd,secOffset=0):
 
 def main():
     overwriteOutFile = False
-    outFile = 'mjdTimesSeq38.npz'
+    outFile = 'mjdTimesSeq2day38.npz'
 
 
     obsSequence0="""
+    051516
+    052018
+    052520
+    """
+    obsSequence1="""
     033323
     033825
     034327
@@ -77,7 +82,7 @@ def main():
     045902
     """
 
-    obsSequence1="""
+    obsSequence2="""
     050404
     050907
     051409
@@ -89,7 +94,7 @@ def main():
     054424
     """
 
-    obsSequence2="""
+    obsSequence3="""
     054926
     055428
     055930
@@ -109,40 +114,76 @@ def main():
     050404
     062942
     """
+
     needPlusOne = needPlusOne.strip().split()
-    obsSequences = [obsSequence0,obsSequence1,obsSequence2]
+    obsSequences = [obsSequence0,obsSequence1,obsSequence2,obsSequence3]
+    wvlCals = ['051341','063518','063518','063518']
 
     #Row coordinate of center of crab pulsar for each obsSequence
-    centersRow = [29,29,10] 
+    centersRow = [9,29,29,10] 
     #Col coordinate of center of crab pulsar for each obsSequence
-    centersCol = [28,30,14] 
+    centersCol = [13,28,30,14] 
 
 
     obsFileNames = []
     obsUtcDate = '20121212'
+    obsUtcDates = ['20121206','20121212','20121212','20121212']
     obsFileNameTimestamps = []
-    for iSeq in range(3):
+    wvlFileNames = []
+    for iSeq in range(len(obsSequences)):
         obsSequence = obsSequences[iSeq]
         obsSequence = obsSequence.strip().split()
         obsFileNameTimestamps.append(obsSequence)
-        obsSequence = [obsUtcDate+'-'+ts for ts in obsSequence]
-        obsFileNames.append([FileName(run='PAL2012',date='20121211',tstamp=ts).obs() for ts in obsSequence])
-    wvlFileName = FileName(run='PAL2012',date='20121211',tstamp='20121212-063518').calSoln()
+        obsUtcDate = obsUtcDates[iSeq]
+        sunsetDate = str(int(obsUtcDate)-1)
+        obsSequence = [obsUtcDates[iSeq]+'-'+ts for ts in obsSequence]
+        obsFileNames.append([FileName(run='PAL2012',date=sunsetDate,tstamp=ts).obs() for ts in obsSequence])
+        wvlCalTstamp = obsUtcDate+'-'+wvlCals[iSeq]
+        wvlFileNames.append(FileName(run='PAL2012',date=sunsetDate,tstamp=wvlCalTstamp).calSoln())
 
     obLists = [[ObsFile(fn)for fn in seq ] for seq in obsFileNames]
     tstampFormat = '%H:%M:%S'
-    print 'fileName','headerUnix','headerUTC','logUnix','packetReceivedUnixTime'
+    #print 'fileName','headerUnix','headerUTC','logUnix','packetReceivedUnixTime'
     for iSeq,obList in enumerate(obLists):
         for iOb,ob in enumerate(obList):
-            ob.loadWvlCalFile(wvlFileName)
+            ob.loadWvlCalFile(wvlFileNames[iSeq])
             ob.setWvlCutoffs(3000,8000)
-#            f = open('/ScienceData/PacketMasterLogs/obs_%s-%s.log'%(obsUtcDate,obsSequences[iSeq].strip().split()[iOb]),'r')
-#            for line in f:
-#                if line.split(' ')[0] == 'bundle':
-#                    tstampLine = line
-#                    break
-#            packetReceivedUnixTimestamp = int(float((tstampLine.split('took')[1].split('total')[0].strip())))
-#            print os.path.split(obsFileNames[iSeq][iOb])[1],time.strftime(tstampFormat,time.gmtime(int(ob.getFromHeader('unixtime')))),time.strftime(tstampFormat,time.gmtime(packetReceivedUnixTimestamp)),packetReceivedUnixTimestamp,ob.checkIntegrity()
+            try:
+                f = open('/ScienceData/PacketMasterLogs/obs_%s-%s.log'%(obsUtcDate,obsSequences[iSeq].strip().split()[iOb]),'r')
+            except:
+                continue
+            lastTstampLines = np.zeros(8)
+            firstTstampLines = np.zeros(8)
+            for line in f:
+                if line.split(' ')[0] == 'bundle':
+                    tstampLine = line
+                    break
+            for line in f:
+                if line.split(' ')[0] == 'bundle':
+                    at = float(line.split('at')[1].split()[0])
+                    lastTstampLine = at
+                    iRoach = int(line.split('roach')[1].split('took')[0].strip())
+                    lastTstampLines[iRoach] = at
+                    if firstTstampLines[iRoach] == 0:
+                        firstTstampLines[iRoach] = at
+                
+#                for line in f:
+#                    if line.split(' ')[0] == 'bundle':
+#                        lastTstampLine = line
+            packetReceivedUnixTimestamp = float((tstampLine.split('took')[1].split('total')[0].strip()))
+#                lastPacketReceivedUnixTimestamp = float(lastTstampLine.split('at')[1].split()[0])
+            firstPacketDelay = packetReceivedUnixTimestamp-int(ob.getFromHeader('unixtime'))
+            #print os.path.split(obsFileNames[iSeq][iOb])[1],time.strftime(tstampFormat,time.gmtime(int(ob.getFromHeader('unixtime')))),time.strftime(tstampFormat,time.gmtime(packetReceivedUnixTimestamp)),packetReceivedUnixTimestamp,lastPacketReceivedUnixTimestamp
+            #print os.path.split(obsFileNames[iSeq][iOb])[1],time.strftime(tstampFormat,time.gmtime(int(ob.getFromHeader('unixtime')))),'%.3f'%firstPacketDelay,'%.2f'%(lastPacketReceivedUnixTimestamp),'%.2f'%(lastPacketReceivedUnixTimestamp+firstPacketDelay)
+#            print os.path.split(obsFileNames[iSeq][iOb])[1],
+#            for i in range(8):
+#                print '%.2f'%firstTstampLines[i],
+#            print ''
+            
+            print os.path.split(obsFileNames[iSeq][iOb])[1],
+            for i in range(8):
+                print '%.2f'%(lastTstampLines[i]+firstPacketDelay-300),
+            print ''
 
     #obsDate = obList[0].getFromHeader('jd')
 
@@ -150,8 +191,9 @@ def main():
     epochMJD = 2400000.5
     secsPerDay = (24.*3600)
 
-    #Time of beginning of observation in mjd
-    obsDate = obLists[0][0].getFromHeader('unixtime')/secsPerDay+unixEpochJD
+    #Time of beginning of observation in mjd, (use beginning of second sequence, second day)
+    obsDate0 = obLists[0][0].getFromHeader('unixtime')/secsPerDay+unixEpochJD
+    obsDate = obsDate0
 
     integrationTime = -1
     firstSec = 0
@@ -160,13 +202,13 @@ def main():
     F1=-3.669005118205e-10#df0/dt Hz/s
     F2=-3.085573120457e-20#d^2f0/dt^2 Hz/s^2
 
-    adjustF0=1-2e-6
+    adjustF0=1#-2e-6
     #adjustF1 = -200 #for seq2
     #adjustF1 = 30 #for all seqs (adjustF0=1.)
     adjustF1 = 1
     #adjustF2 = 1e7
-    adjustF2 = 1
-    freq0 = adjustF0*calculateFreq(obsDate-epochMJD,firstSec)*secsPerDay
+    adjustF2 = 0
+    freq0 = adjustF0*calculateFreq(obsDate0-epochMJD,firstSec)*secsPerDay
     freqDot = adjustF1*F1*secsPerDay**2#convert to days
     freqDotDot = adjustF2*F2* secsPerDay**3
 
@@ -204,10 +246,19 @@ def main():
                 jdTimes = np.append(jdTimes,jdTimesOb)
         mjdTimes = jdTimes - epochMJD #convert to modified jd
         np.savez(outFile,mjdTimes=mjdTimes,mjdTimesList=mjdTimesList)
-    mjdTime0 = obsDate-epochMJD
+    mjdTime0 = obsDate0-epochMJD
 
+    needPlusOneMask = []
+    for iSeq,obList in enumerate(obLists):
+        for iOb,ob in enumerate(obList):
+            if obsFileNameTimestamps[iSeq][iOb] in needPlusOne:
+                needPlusOneMask.append(1)
+            else:
+                needPlusOneMask.append(0)
 
     phaseOffset = 0
+    print freq0
+    print mjdTime0
     totalPhases = freq0*(mjdTimes-mjdTime0)+freqDot*(mjdTimes-mjdTime0)**2/2.+freqDotDot*(mjdTimes-mjdTime0)**3/6.+phaseOffset
 
     totalPhasesList = []
