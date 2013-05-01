81d30ee0fbb34ec8aad99f424c459bddfebdcd0f
PSzypryt <pszypryt@physics.ucsb.edu>
2013-05-01 10:21:03 -0700

    Modified hour angle offset in CentroidCalc.py.  Changed calculateCR.py to use a manually inserted array of x-y values.

--- astrometry/calculateCR.py
+++ astrometry/calculateCR.py
@@ -17,6 +17,10 @@ import matplotlib.cm as cm
 times, x, y, ha =np.loadtxt('/home/pszypryt/Scratch/centroid_test/centroid_list.txt',unpack='True',skiprows=1)  
 numberOfPoints = len(x)
 
+
+x=np.array([37.7630968422, 37.59877992885, 37.236499667099999, 37.177924051299996, 36.783136532100002, 36.271422505549999, 35.78070321165, 35.442410522149999, 35.037066280399998, 34.461779605399997, 33.566525930650002, 32.026794121449996, 31.579013769550002])
+y=np.array([19.826419748699998, 20.511769704350002, 21.3182756912, 21.878565210700003, 22.35456673885, 23.730502243049997, 24.615149849150001, 24.944072180550002, 25.500067872800003, 26.695203749000001, 27.6978730437, 29.844366393850002, 30.4193377876])
+
 meanX = x.mean()
 meanY = y.mean()
 
@@ -58,8 +62,8 @@ print 'MeanY = ' + str(meanY)
 centerX = -(((-sumUV*sumVUU)+(sumUUU*sumVV)+(sumUVV*sumVV)+(-sumUV*sumVVV))/(2.0*((sumUV*sumUV) -sumUU*sumVV)))
 centerY = -(((-sumUUU*sumUV)+(-sumUV*sumUVV)+(sumUU*sumVUU)+(sumUU*sumVVV))/(2.0*((sumUV*sumUV) -sumUU*sumVV)))
 
-print 'CenterX = ' + str(centerX)
-print 'CenterY = ' + str(centerY)
+#print 'CenterX = ' + str(centerX)
+#print 'CenterY = ' + str(centerY)
 
 alpha = (centerX*centerX)+(centerY*centerY)+((sumUU+sumVV)/numberOfPoints)
 
@@ -70,12 +74,18 @@ centerY = centerY + meanY
 radius = np.sqrt(alpha)
 print 'CenterX = ' + str(centerX) + ' CenterY = ' + str(centerY) + ' Radius = ' + str(radius)
 
+fig = plt.figure()
+circle=plt.Circle((centerX,centerY),radius,fill=False)
+fig.gca().add_artist(circle)
+'''
 xFitP = np.linspace(centerX-radius,centerX+radius,500)
 xFitN = xFitP[::-1]
 yFitP = centerY + np.sqrt(radius*radius-(xFitP-centerX)*(xFitP-centerX))
 yFitN = centerY - np.sqrt(radius*radius-(xFitN-centerX)*(xFitN-centerX))
 xFit = np.append(xFitP,xFitN)
 yFit = np.append(yFitP,yFitN)
-
-plt.plot(x,y,'.',xFit,yFit)
+'''
+plt.plot(x,y,'b.', centerX,centerY,'ro')
+plt.xlim([0,43])
+plt.ylim([0,45])
 plt.show()
