95dd947ddeccc96aa6306b47e86d63ee356b1299
Alex Kirchner <Alex.kirchner.523@gmail.com>
2013-07-18 13:31:58 -0700

    correct poisson distribution plot

--- examples/Pal2012-cosmicmask/CosmicMaskExample.py
+++ examples/Pal2012-cosmicmask/CosmicMaskExample.py
@@ -16,8 +16,8 @@ sundownDate = '20121211'
 obsDate = '20121212'
 seq = '121229'
 fn = FileName.FileName(run, sundownDate, obsDate+"-"+seq)
-cosmic = Cosmic(fn, beginTime=123, endTime=124)
-dictionary0 = cosmic.findCosmics(threshold=30, populationMax=1000)
+cosmic = Cosmic(fn, beginTime=123, endTime=133)
+dictionary0 = cosmic.findCosmics(nSigma=6, threshold=15, populationMax=1000)
 
 
 interval = dictionary0['interval']
@@ -26,7 +26,9 @@ ObsFile.ObsFile.writeCosmicIntervalToFile(interval,
                                           'junk.h5')
 
 cosmic.file.loadCosmicMask('junk.h5')
-dictionary1 = cosmic.findCosmics(threshold=30, populationMax=1000)
+dictionary1 = cosmic.findCosmics(nSigma=6, threshold=15, populationMax=1000)
+nSigma = 6
+threshold = 15
 
 plt.clf()
 hist0 = dictionary0['populationHg'][0]
@@ -37,21 +39,18 @@ hist1 = dictionary1['populationHg'][0]
 bins1 = np.arange(len(hist1))
 plt.plot(bins1, hist1, label="cosmic mask")
 
-mu1 = (bins1*hist1).sum()/float(bins1.sum())
-print "mu1=",mu1
+mu1 = (bins1*hist1).sum()/float(hist1.sum())
+print "mu1=", mu1
 p = poisson(mu1)
 xvalues = np.arange(20)
 theory = hist1.sum()*p.pmf(xvalues)
-print "xvalues=",xvalues
-print "theory=",theory
-plt.plot(xvalues,theory,'+',label="poisson with mu=%.3f"%mu1)
-
-#s = np.random.poisson(np.mean(dictionary1['populationHg'][0]), 1000)
-#plt.plot(s, hist1)
+plt.plot(xvalues, theory,'+',label="poisson with mu=%.3f"%mu1)
 
 plt.xscale('log')
 plt.yscale('symlog',linthreshy=0.5)
 plt.legend()
-plt.savefig("testCosmicTimeMasking.png")
+plt.title("Ten Seconds of Data;"+"  "+"threshold=%.3f"%threshold+"  "+ 
+          "nSigma=%.3f"%nSigma)
+plt.savefig("CosmicTimeMasking.png")
 
         
