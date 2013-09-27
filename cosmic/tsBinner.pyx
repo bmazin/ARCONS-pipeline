5b59eeaef81c1481afc5e841192cbd72c4bcaeb2
Chris Stoughton <chris.stoughton@gmail.com>
2013-09-27 10:11:34 -0700

    add 32 bit method

--- cosmic/tsBinner.pyx
+++ cosmic/tsBinner.pyx
@@ -20,4 +20,20 @@ def tsBinner(np.ndarray[np.uint64_t, ndim=1] tstamps, \
     return
 
     
+def tsBinner32(np.ndarray[np.uint32_t, ndim=1] tstamps, \
+    np.ndarray[np.int_t, ndim=1] bins):
+    """
+    bin the values of the tstamps array into the bins array.
+
+    tstamps array is of type np.uint32
+
+    bins array is of type int
+    """
+    cdef int nTStamps = tstamps.shape[0]
+    cdef int iTStamp
+    for iTStamp in range(nTStamps):
+        bins[tstamps[iTStamp]] += 1
+    return
+
+    
     
