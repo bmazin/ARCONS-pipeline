5f0f2b3ba01eec3381a5502e3e093d44bcefa573
Chris Stoughton <chris.stoughton@gmail.com>
2013-10-08 19:46:43 -0700

    add some comments

--- examples/cosmic-tuning/cosmic-second-boundary.py
+++ examples/cosmic-tuning/cosmic-second-boundary.py
@@ -16,11 +16,6 @@ import logging
 run = 'PAL2012'
 sundownDate = '20121211'
 obsDate = '20121212'
-# December 11
-# Final sequence, toward end of run, thin high clouds at around 12:50, moved to confirm position at '122234', also at '130752' at 125s. (16,15)
-
-#seq5 = ['112709', '113212', '113714', '114216', '114718', '115220', '115722', '120224', '120727', '121229', '121732', '122234', '122736', '123238', '123740', '124242', '124744', '125246', '125748', '130250', '130752', '131254', '131756', '132258', '132800', '133303']
-
 seq = '121229'
 
 populationMax=1000
@@ -32,8 +27,14 @@ nSigma = 10
 stride = 10
 threshold = 15
 
-bin0 = 23
-print "bin0=",bin0
+# test the theory that that extra photons show up 23 clock ticks
+# after the beginning of each second
+bin0 = 23 
+
+# find cosmics and put the results in the dictionary fc.
+# fc['timeHgvalues'] is an array.  The ith value of the array
+# is the number of photons on the array during the ith clock tick after 
+# beginTime
 
 fn = FileName(run, sundownDate, obsDate+"-"+seq)
 cosmic = Cosmic(fn, beginTime=beginTime, endTime=endTime, 
@@ -44,13 +45,18 @@ fc = cosmic.findCosmics(stride=stride,
                         populationMax=populationMax,
                         nSigma=nSigma)
 
+# list of x values for plotting
 tList = []
 for t in range(beginTime,int(cosmic.endTime)):
     tList.append(t)
 
-nPhotonLists = {}
+# count the number of photons each second, with different offsets from bin0
 offsets = [0, 100, 1234, 54321]
 
+# dictionary of y values to plot
+nPhotonLists = {}
+
+# do actual work here
 for offset in offsets:
     nPhotonLists[offset] = []
     for t in range(beginTime,int(cosmic.endTime)):
@@ -58,6 +64,7 @@ for offset in offsets:
         nPhoton = fc['timeHgValues'][bin-2:bin+3].sum()
         nPhotonLists[offset].append(fc['timeHgValues'][bin])
 
+# make a plot
 plt.clf()
 for offset in offsets:
     plt.plot(tList, nPhotonLists[offset], label="offset=%d"%offset)
@@ -67,4 +74,5 @@ plt.xlabel("time")
 plt.ylabel("number of photons")
 plt.ylim(ymin=-2)
 plt.savefig("cosmic-second-boundary.png")
+
 del cosmic
