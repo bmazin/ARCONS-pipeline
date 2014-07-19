Bug fixes: astrometry .h5 headers, flux/flat weights, and post-calibration hot pixel masking.

astrometry/CalculateRaDec.py
@@ -28,8 +28,9 @@ class CalculateRaDec:
     def __init__(self,centroidListFile):
         '''
         centroidListFile may either be a pathname (string) to a centroid list file, a PyTables instance
-        of a centroid list file, or a PyTables instance of the actual centroid list node (table) within
+        of a centroid list file, or a PyTables instance of the actual centroid list root node within
         a centroid list file (as stored in PhotList instances).
+        Note the last option has changed slightly! (JvE 7/18/2014)
         '''
         
         if type(centroidListFile) is str:
@@ -46,31 +47,35 @@ class CalculateRaDec:
                 #return
             # Load centroid positions, center of rotation, hour angle, and RA/DEC data from centroidListFile.
             clFile = tables.openFile(fullCentroidListFileName, mode='r')
+            clRoot = clFile.root
             centroidListNode = clFile.root.centroidlist
         elif type(centroidListFile) is tables.file.File:
             clFile = None
+            clRoot = centroidListFile.root
             centroidListNode = centroidListFile.root.centroidlist
         elif type(centroidListFile) is tables.group.Group:
             clFile = None
-            centroidListNode = centroidListFile
+            clRoot = centroidListFile 
+            centroidListNode = centroidListFile.centroidlist
         else:
-            raise ValueError('Input parameter centroidListFile must be either a pathname string or a PyTables file instance.')
+            raise ValueError('''Input parameter centroidListFile must be either a pathname string,
+                                a PyTables file instance, or a PyTables node instance''')
         
-        try:
-            header = clFile.root.header.header
+        if 'header' in clRoot:
+            header = clRoot.header.header
             titles = header.colnames
             info = header[0]
             self.nRow = info[titles.index('nRow')]
             self.nCol = info[titles.index('nCol')]
             self.centroidRightAscension = info[titles.index('RA')]
             self.centroidDeclination = info[titles.index('Dec')]
-        except:
+        else:
+            #For back-compatibility purposes
             self.params = np.array(centroidListNode.params.read())
             self.nRow = int(self.params[0])
             self.nCol = int(self.params[1])
             self.centroidRightAscension = self.params[2]
             self.centroidDeclination = self.params[3]
-
 
         self.times = np.array(centroidListNode.times.read())
         self.hourAngles = np.array(centroidListNode.hourAngles.read())

photonlist/RADecImage.py
@@ -39,7 +39,8 @@ class RADecImage(object):
     '''
     
     def __init__(self,photList=None,nPixRA=None,nPixDec=None,cenRA=None,cenDec=None,
-                 vPlateScale=0.1, detPlateScale=None, firstSec=0, integrationTime=-1):
+                 vPlateScale=0.1, detPlateScale=None, firstSec=0, integrationTime=-1,
+                 doWeighted=True,wvlMin=None,wvlMax=None):
                  #expWeightTimeStep=1.0):
         '''
         Initialise a (possibly empty) RA-dec coordinate frame image.
@@ -61,6 +62,8 @@ class RADecImage(object):
                         begin integration 
             integrationTime: float, length of time to integrate for in seconds. If
                         -1, integrate to end of photon list.
+            doWeighted,wvlMin,wvlMax - passed on to loadImage() if 'photList' keyword
+                        is provided. Otherwise ignored.
             
             #### DEPRECATED ##### 
             expWeightTimeStep: float, time step to use when calculating exposure
@@ -103,7 +106,8 @@ class RADecImage(object):
             and nPixRA is not None and nPixDec is not None):
             self.setCoordGrid()            
         if photList is not None:
-            self.loadImage(photList,firstSec=firstSec,integrationTime=integrationTime)     
+            self.loadImage(photList,firstSec=firstSec,integrationTime=integrationTime,
+                           doWeighted=doWeighted,wvlMin=wvlMin,wvlMax=wvlMax)     
     
     
     def setCoordGrid(self):
@@ -313,7 +317,10 @@ class RADecImage(object):
         #tStartFrames = np.arange(start=firstSec,stop=lastSec,
         #                         step=self.expWeightTimeStep)
         #tEndFrames = (tStartFrames+self.expWeightTimeStep).clip(max=lastSec)    #Clip so that the last value doesn't go beyond the end of the exposure.
-        tStartFramesAll = np.array(photList.file.root.centroidList.times.read()) #Convert to array, since it's saved as a list.
+        if 'centroidlist' in photList.file.root.centroidList:
+            tStartFramesAll = np.array(photList.file.root.centroidList.centroidlist.times.read()) #Convert to array, since it's saved as a list.
+        else:
+            tStartFramesAll = np.array(photList.file.root.centroidList.times.read())   #For back compatibility
         tEndFramesAll = np.append(tStartFramesAll[1:], np.inf)                   #Last frame goes on forever as far as we know at the moment
         withinIntegration = ((tStartFramesAll < lastSec) & (tEndFramesAll > firstSec))
         tStartFrames = tStartFramesAll[withinIntegration].clip(min=firstSec)   #Now clip so that everything is within the requested integration time.
@@ -483,7 +490,7 @@ class RADecImage(object):
         
         if logScale is True: toDisplay = np.log10(toDisplay)
         
-        if image is not None: toDisplay = image
+        if image is not None: toDisplay = np.copy(image)
         
         #####################
         #Rotate by 180deg so north is up and east is left - just a fudge for now, need to
@@ -502,12 +509,32 @@ class RADecImage(object):
         #raMin = (self.gridRA[0:-1] + self.gridRA[1:])/2.0 / np.pi * 180.
         #dec = (self.gridDec[0:-1] + self.gridDec[1:])/2.0 / np.pi * 180.
         
-        utils.plotArray(toDisplay,cbar=True,normMin=normMin,normMax=normMax,
-                        colormap=colormap,plotFileName=fileName,
-                        showMe=(type(fileName) is not str))
-        #mpl.imshow(toDisplay,vmin=normMin,vmax=normMax, extent=(180./np.pi)*
-        #           np.array([self.gridRA[0],self.gridRA[-1],self.gridDec[0],self.gridDec[-1]])
-
+        #utils.plotArray(toDisplay,cbar=True,normMin=normMin,normMax=normMax,
+        #                colormap=colormap,plotFileName=fileName,
+        #                showMe=(type(fileName) is not str))
+        #fig = mpl.figure()
+        #ax = fig.add_subplot(111)
+        #ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
+        mpl.ticklabel_format(style='plain',useOffset=False)
+        mpl.tick_params(direction='out')
+        mpl.imshow(toDisplay,vmin=normMin,vmax=normMax, extent=(180./np.pi)*
+                    np.array([self.gridRA[0],self.gridRA[-1],self.gridDec[0],self.gridDec[-1]]),
+                    origin='upper', cmap=colormap)
+        #mpl.ticklabel_format(style='plain',useOffset=False)
+ 
+        ax = mpl.gca()
+        xtickBase = 10**(np.round(np.log10((self.gridRA[-1]-self.gridRA[0])*180./np.pi/10.)))
+        ytickBase = 10**(np.round(np.log10((self.gridDec[-1]-self.gridDec[0])*180./np.pi/10.)))
+        ax.xaxis.set_major_locator(mpl.MultipleLocator(base=xtickBase))
+        ax.yaxis.set_major_locator(mpl.MultipleLocator(base=ytickBase))
+        mpl.xticks(rotation=90)
+        mpl.xlabel('R.A. (deg)')
+        mpl.ylabel('Dec. (deg)')
+        
+        #ax.xaxis.set_major_locator(mpl.MultipleLocator(base=0.001))
+        #ax.yaxis.set_major_locator(mpl.MultipleLocator(base=0.001))
+        #mpl.show()
+        
         
     def writeFits(self, fileName='RADecImage.fits', expWeight=True):
         '''

photonlist/photlist.py
@@ -117,7 +117,9 @@ class PhotList(object):
         
     
     def getImageDet(self,firstSec=0,integrationTime=-1,wvlMin=-np.Inf,
-                 wvlMax=np.Inf,newMethod=True):
+                 wvlMax=np.Inf,doWeighted=True,
+                 #newMethod=True
+                 ):
         '''
         Return a 2D image consisting of photon counts in each pixel, in the detector pixel
         coordinate frame.
@@ -126,10 +128,16 @@ class PhotList(object):
             firstSec - start time for integration within photon list object (in sec)
             integrationTime - time to integrate for in seconds. If -1, integrates to end of file.
             wvlMin,wvlMax - minimum and maximum wavelengths to include in the integration (Angstroms) 
-            newMethod=True to use numpy histogram for creating image. Runs faster for short integration times,
-                      but just a little slower for full images.
+            doWeighted - True to apply flatcal and fluxcal weights.
+            
+            ----DEPRECATED, NO LONGER FUNCTIONAL - always uses new method (Jul 9 2014)------
+            newMethod - True to use numpy histogram for creating image. Runs faster for short integration times,
+                        but just a little slower for full images.
+            --------------------------------------------------------------------------------
+            
         OUPUTS:
             A 2D image array matching the shape/size of the detector.
+            
         '''
         
         if integrationTime==-1:
@@ -137,65 +145,47 @@ class PhotList(object):
         else:  
             lastSec = firstSec+integrationTime
 
-        if newMethod:
-            #Should be more efficient....
-            print 'Searching photons'
-            #ind = self.photTable.getWhereList('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')
-            tic = time.clock()
-            photons = self.photTable.readWhere('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')
-            xPix = photons['xPix']
-            yPix = photons['yPix']
-            print 'Time taken (s): ',time.clock()-tic
-            print 'Doing by second method'
-            #tic = time.clock()
-            #photons2 = [x for x in self.photTable.iterrows() if (x['arrivalTime']>=firstSec) and (x['arrivalTime']<lastSec)
-            #            and x['wavelength'>=wvlMin] and x['wavelength']<wvlMax]
-            #print 'Time taken (s): ',time.clock()-tic
-            #print 'Doing by third method'
-            #tic = time.clock()
-            #photons2 = [(phot['xPix'], phot['yPix']) for phot in self.photTable.where('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')]
-            #photons2 = self.photTable.getWhereList('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')
-            #xPix2 = self.photTable[photons2]['xPix']
-            #yPix2 = self.photTable[photons2]['yPix']
-            #print 'Time taken (s): ',time.clock()-tic
-             
-            #print 'Getting coordinates'
-            #photX = self.photTable.readCoordinates(ind,field='xPix')
-            #photY = self.photTable.readCoordinates(ind,field='yPix')
-            print 'Building 2D histogram'
-            image, y,x = np.histogram2d(yPix,xPix,bins=[self.nRow,self.nCol],range=[[-1,self.nRow],[-1,self.nCol]])
-            #image, y,x = np.histogram2d(photX,photY,bins=[self.nRow,self.nCol],range=[[-1,self.nRow],[-1,self.nCol]])
-            #image2, y,x = np.histogram2d(yPix2,xPix2,bins=[self.nRow,self.nCol],range=[[-1,self.nRow],[-1,self.nCol]])
-            #assert np.all(image1 == image2)
-
+#         if newMethod:
+        #Should be more efficient....
+        print 'Searching photons...'
+        #ind = self.photTable.getWhereList('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')
+        tic = time.clock()
+        photons = self.photTable.readWhere('(arrivalTime >= firstSec) & (arrivalTime < lastSec) & (wavelength >= wvlMin) & (wavelength < wvlMax)')
+        xPix = photons['xPix']
+        yPix = photons['yPix']
+        if doWeighted is True:
+            photWeights = photons['flatWeight'] * photons['fluxWeight']   #********EXPERIMENTING WITH ADDING FLUX WEIGHT - NOT FULLY TESTED, BUT SEEMS OKAY....********
         else:
-            #Initialise an empty image
-            image = np.empty((self.nRow,self.nCol),dtype=float)
-            image.fill(np.NaN)
-           
-            #Step through pixels and fill em up.
-            for iRow in range(self.nRow):    #self.nCol):
-                print 'Reading pixel row ', iRow
-                for iCol in range(self.nCol):
-                    rowcol = xyPack(iRow,iCol)
-                    #image[iRow,iCol] = len(self.photTable.getWhereList('(Xpix == iCol) & (Ypix == iRow) & (ArrivalTime >= firstSec) & (ArrivalTime < lastSec)')) # & (Wavelength >= wvlMin) & (Wavelength < wvlMax)' ))
-                    #image[iRow,iCol] = len(self.photTable.getWhereList(('XYpix==rowcol'))) # & (self.photTable.cols.Ypix == iRow) & (self.photTable.cols.ArrivalTime >= firstSec)
-                                                                        #& (self.photTable.cols.ArrivalTime < lastSec) & (self.photTable.cols.Wavelength >= wvlMin) & (self.photTable.cols.Wavelength < wvlMax) ))
-    
-                    photons = (self.photTable.readWhere('xyPix==rowcol'))
-                    image[iRow,iCol] = np.sum((photons['arrivalTime'] >= firstSec) & (photons['arrivalTime'] < lastSec)
-                                              & (photons['wavelength'] >= wvlMin) & (photons['wavelength'] < wvlMax))
-                    #rowcolplus=rowcol+1
-                    #rowcolminus=rowcol-1
-                    #arrTimes = self.photTable.getWhereList('(xyPix<rowcolplus) & (xyPix>rowcolminus)')
-                    #image[iRow,iCol] = np.sum((arrTimes >= firstSec) & (arrTimes < lastSec))
-                    
-                    #image[iRow,iCol] = len(photons.where('ArrivalTime >= firstSec) & ArrivalTime < lastSec)'))
-                    #image[iRow,iCol] = len(photons)
-                    
-                    #image[iRow,iCol] = len(self.photTable.getWhereList('(ArrivalTime >= firstSec) & (ArrivalTime < lastSec) & (Ypix == iRow) & (Xpix == iCol)')) # & (Wavelength >= wvlMin) & (Wavelength < wvlMax)' ))                
-                    #image[iRow,iCol] = len(self.photTable.getWhereList('(Ypix == iRow)' ))
-                
+            photWeights = None
+        print 'Time taken so far (s): ',time.clock()-tic
+        print 'Building 2D histogram'
+        image, y,x = np.histogram2d(yPix,xPix,bins=[self.nRow,self.nCol],range=[[-1,self.nRow],[-1,self.nCol]],
+                                    weights=photWeights)
+        print 'Done - total time taken (s): ',time.clock()-tic
+
+#         else:
+#             #Initialise an empty image
+#             image = np.empty((self.nRow,self.nCol),dtype=float)
+#             image.fill(np.NaN)
+# 
+#             #Step through pixels and fill em up.
+#             print 'Looping through pixels and counting up...'
+#             tic = time.clock()
+#             for iRow in range(self.nRow):    #self.nCol):
+#                 print 'Reading pixel row ', iRow
+#                 for iCol in range(self.nCol):
+#                     rowcol = xyPack(iRow,iCol)
+#                     photons = (self.photTable.readWhere('xyPix==rowcol'))
+#                     if doWeighted is True:
+#                         photWeights = photons['flatWeight'] * photons['fluxWeight']   #********EXPERIMENTING WITH ADDING FLUX WEIGHT - NOT FULLY TESTED, BUT SEEMS OKAY....********
+#                     else:
+#                         photWeights = np.ones(len(photons))     #Set all weights to one if doWeighted is not True.
+#                     inRange = ((photons['arrivalTime'] >= firstSec) & (photons['arrivalTime'] < lastSec)
+#                                 & (photons['wavelength'] >= wvlMin) & (photons['wavelength'] < wvlMax))
+#                     
+#                     image[iRow,iCol] = np.sum(photWeights[inRange])
+#             print 'Done - total time taken (s): ',time.clock()-tic
+
         #That should be it....
         return image
 
@@ -350,7 +340,7 @@ def writePhotonList(obsFile, filename=None, firstSec=0, integrationTime=-1,
         plFile.copyNode(obsFile.file.root.beammap, newparent=plFile.root, newname='beammap', recursive=True)
         plFile.copyNode(obsFile.file.root.header, newparent=plFile.root, recursive=True)
         if obsFile.centroidListFile is not None:
-            plFile.copyNode(obsFile.centroidListFile.root.centroidlist, newparent=plFile.root, newname='centroidList', recursive=True)
+            plFile.copyNode(obsFile.centroidListFile.root, newparent=plFile.root, newname='centroidList', recursive=True)
         if obsFile.timeAdjustFile is not None:
             #If there's any time adjustment file associated, store that too in the photon list 
             #file, and also correct the exptime in the output header info (which is generally not updated in the original obs file).