3bc5eafa9b02ac0eadb50cd83e002672ca38ee8e
Julian van Eyken <vaneyken@physics.ucsb.edu>
2014-08-04 18:04:03 -0700

    Bug fix to util/test/loadTestObsFile.py

--- util/test/loadTestObsFile.py
+++ util/test/loadTestObsFile.py
@@ -3,7 +3,7 @@ from util import FileName as fn
 from util import ObsFile as of
 
 def loadTestObsFile(obsFileName=None, wvlCalFileName=None, flatCalFileName=None,
-                    fluxCalFileName=None, hotPixFileName=None):
+                    fluxCalFileName=None, hotPixFileName=None, timeAdjFileName=None):
     '''
     Quick function to load up a test obs file and all its calibrations, and return
     the resulting ObsFile object.
@@ -15,7 +15,7 @@ def loadTestObsFile(obsFileName=None, wvlCalFileName=None, flatCalFileName=None,
     run = 'PAL2012'
     date = '20121208'
     if obsFileName is None: obsFileName = fn.FileName(run=run,date=date,tstamp='20121209-044636').obs()   #'obs_20121209-044636.h5'
-    if timeAdjFileName is None: timeAdjFileName = FileName(run='PAL2012').timeAdjustments()
+    if timeAdjFileName is None: timeAdjFileName = fn.FileName(run='PAL2012').timeAdjustments()
     if wvlCalFileName is None: wvlCalFileName = fn.FileName(run=run, date=date, tstamp='20121209-060704').calSoln()    #'calsol_20121209-060704.h5'
     if flatCalFileName is None: flatCalFileName = fn.FileName(run=run, date='20121210').flatSoln()    #'flatsol_20121210.h5'
     if fluxCalFileName is None: fluxCalFileName = fn.FileName(run=run, date=date, tstamp='20121209-020416').fluxSoln()
@@ -26,7 +26,7 @@ def loadTestObsFile(obsFileName=None, wvlCalFileName=None, flatCalFileName=None,
     print 'Loading obs file and calibrations:'
     print obsFileName
     obsFile = of.ObsFile(obsFileName)
-    obsFile.loadTimeAdjustmentsFile(timeAdjFileName)
+    obsFile.loadTimeAdjustmentFile(timeAdjFileName)
     print timeAdjFileName
     obsFile.loadBestWvlCalFile()
     print obsFile.wvlCalFileName
