5305339e7245cda8c1c7f5e4c98d3b40755c1a57
Chris Stoughton <chris.stoughton@gmail.com>
2015-02-18 13:33:34 -0600

    first version

---
+++ examples/PAL2014-PSN234416/ofs1.py
@@ -0,0 +1,14 @@
+from util.ObsFileSeq import ObsFileSeq
+from util.readDict import readDict
+
+d = readDict()
+d.read_from_file("mosaic.dict")
+name = 'PSN234416a'
+dt = 200
+ofs = ObsFileSeq(name, d['run'], d['date'], d['timeStampList'], dt)
+#ofs.plotLocations(name+".png")
+wvMin = 3000
+wvMax = 13000
+#ofs.makeAllFitsFiles(wvMin, wvMax)
+sc0 = ofs.getSpectralCubeByFrame(0)
+del ofs
