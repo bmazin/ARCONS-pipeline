348ece8d409b592ee8ca0651d7f9a24b0809d0f0
Chris Stoughton <chris.stoughton@gmail.com>
2015-02-18 11:54:39 -0800

    load beammap before wavecal

--- examples/PAL2014-PSN234416/test1.py
+++ examples/PAL2014-PSN234416/test1.py
@@ -6,9 +6,9 @@ timeStamp = '20141023-033821'
 fn = FileName(run,date,timeStamp)
 
 of = ObsFile(fn.obs())
+of.loadBeammapFile(fn.beammap())
 of.loadBestWvlCalFile()
 print "wvlCalFileName=",of.wvlCalFileName
-of.loadBeammapFile(fn.beammap())
 fn2 = FileName(run,date,"")
 of.loadFlatCalFile(fn2.flatSoln())
 row = 4
@@ -17,4 +17,4 @@ firstSec = 72
 integrationTime = 73
 spec = of.getPixelSpectrum(row,col,firstSec,integrationTime)
 print "spec=",spec
-
+del of
