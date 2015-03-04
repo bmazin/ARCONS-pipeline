0a41535965374e035c6610a27661303c26c4e443
Matt Strader <mstrader@physics.ucsb.edu>
2015-03-04 15:36:49 -0800

    changed to use the param file for list of obs

--- examples/Pal2014-J0337/binPhaseProfiles.py
+++ examples/Pal2014-J0337/binPhaseProfiles.py
@@ -8,6 +8,8 @@ from photonlist.photlist import writePhotonList,PhotList
 from headers.ArconsHeaders import PulsarPhotonList
 from timing import photonTiming
 import ephem
+import functools
+from util.readDict import readDict
 
 RADIANS_PER_ARCSEC = np.pi/648000.
 
@@ -16,22 +18,21 @@ def plotPulseProfile(ax,phaseBinEdges,pulseProfile,**kwargs):
     doublePulseProfile = np.append(pulseProfile,pulseProfile)
     ax.plot(doublePhaseBinEdges,np.append(doublePulseProfile,doublePulseProfile[-1]),drawstyle='steps-post',**kwargs)
 
-def foldPL(plPath,centroidRaStr='03:37:43.826',centroidDecStr='14:15:14.828',apertureRadius=1.,nPhaseBins=160):
+def foldPl(plPath,centroidRaStr='03:37:43.826',centroidDecStr='14:15:14.828',apertureRadius=1.,nPhaseBins=150,wvlRange=(3000,13000)):
     print 'folding',plPath
     centroidRa = float(ephem.hours(centroidRaStr))
     centroidDec = float(ephem.degrees(centroidDecStr))
     pl = PhotList(plPath)
     apertureRadius *= RADIANS_PER_ARCSEC
+    wvlStart,wvlEnd = wvlRange
     print 'reading photons'
-    photons = pl.photTable.readWhere('sqrt((ra-centroidRa)**2. + (dec-centroidDec)**2.) < apertureRadius')
+    photons = pl.photTable.readWhere('(sqrt((ra-centroidRa)**2. + (dec-centroidDec)**2.) < apertureRadius) & (wvlStart < wavelength) & (wavelength < wvlEnd)')
     
     print 'binning phases'
     del pl
 
     phases = photons['pulsePhase']
-    print phases[0:10]
     phaseProfile,phaseBinEdges = np.histogram(phases,bins=nPhaseBins,range=(0.,1.))
-    print phaseProfile
     profileErrors = np.sqrt(phaseProfile)
     print 'done folding'
     del photons,phases
@@ -40,116 +41,29 @@ def foldPL(plPath,centroidRaStr='03:37:43.826',centroidDecStr='14:15:14.828',ape
 
 if __name__=='__main__':
     savePath = '/Scratch/dataProcessing/J0337/'
-    run = 'PAL2014'
+    paramFile = 'j0337.dict'
+    params = readDict()
+    params.read_from_file(paramFile)
+    run = params['run']
     sunsetDates = []
     flatDates = []
     obsSequences = []
     parFiles = []
+
+    sunsetDates.append(params['sunsetDate0'])
+    flatDates.append(params['flatDate0'])
+    parFiles.append(params['parFile0'])
+    obsSequences.append(params['obsSequence0'])
     
+    sunsetDates.append(params['sunsetDate1'])
+    flatDates.append(params['flatDate1'])
+    parFiles.append(params['parFile1'])
+    obsSequences.append(params['obsSequence1'])
 
-    obsSequence0 = [
-    '20140925-111532',
-    '20140925-112035',
-    '20140925-112538',
-    '20140925-113041',
-    '20140925-113544',
-    '20140925-114047',
-    '20140925-114550',
-    '20140925-115053',
-    '20140925-115605',
-    '20140925-120112',
-    '20140925-120615',
-    '20140925-121118',
-    '20140925-121621',
-    '20140925-122124',
-    '20140925-122627',
-    '20140925-123130',
-    '20140925-123633'
-    ]
-    sunsetDates.append('20140924')
-    flatDates.append('20140924')
-    obsSequences.append(obsSequence0)
-    parFiles.append('J0337-mjd-56924.par')
-    
-
-    obsSequence1 = [
-    '20140926-081832',
-    '20140926-082336',
-    '20140926-082840',
-    '20140926-083344',
-    '20140926-083848',
-    '20140926-084352',
-    '20140926-084856',
-    '20140926-085400',
-    '20140926-085904',
-    '20140926-090408',
-    '20140926-090912',
-    '20140926-091416',
-    '20140926-091920',
-    '20140926-092424',
-    '20140926-092928',
-    '20140926-093432',
-    '20140926-093936',
-    '20140926-094440',
-    '20140926-094944',
-    '20140926-095448',
-    '20140926-095952',
-    '20140926-100456',
-    '20140926-101000',
-    '20140926-101504',
-    '20140926-102008',
-    '20140926-102512',
-    '20140926-103016',
-    '20140926-103520',
-    '20140926-104024',
-    '20140926-104528'
-    ]
-    sunsetDates.append('20140925')
-    flatDates.append('20140924')
-    obsSequences.append(obsSequence1)
-    parFiles.append('J0337-mjd-56924.par')
-
-    obsSequence2 = [
-    '20141021-063246',
-    '20141021-063750',
-    '20141021-064253',
-    '20141021-064758',
-    '20141021-065303',
-    '20141021-065807',
-    '20141021-070311',
-    '20141021-071138',
-    '20141021-071656',
-    '20141021-072200',
-    '20141021-072704',
-    '20141021-073208',
-    '20141021-073713',
-    '20141021-074216',
-    '20141021-074720',
-    '20141021-075225',
-    '20141021-075729',
-    '20141021-080232',
-    '20141021-080737',
-    '20141021-081242',
-    '20141021-081747',
-    '20141021-082252',
-    '20141021-082755',
-    '20141021-083300',
-    '20141021-083805',
-    '20141021-084310',
-    '20141021-084814',
-    '20141021-085318',
-    '20141021-085822',
-    '20141021-090326',
-    '20141021-090832',
-    '20141021-091336',
-    '20141021-091840',
-    '20141021-092345',
-    '20141021-092851'
-    ]
-    sunsetDates.append('20141020')
-    flatDates.append('20141020')
-    obsSequences.append(obsSequence2)
-    parFiles.append('J0337-mjd-56951.par')
+    sunsetDates.append(params['sunsetDate2'])
+    flatDates.append(params['flatDate2'])
+    parFiles.append(params['parFile2'])
+    obsSequences.append(params['obsSequence2'])
 
 
     plPaths = []
@@ -161,12 +75,20 @@ if __name__=='__main__':
     obsTimestamps = np.concatenate(obsSequences)
     plPaths = [path for pathList in plPaths for path in pathList]
 
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count()//2)
-    outDicts = pool.map(foldPL,plPaths)
+    nPhaseBins = 150 #bin it finely, we can rebin later
+    wvlStart = 3000 #angstrom
+    wvlEnd = 8000 #angstrom
+    wvlRange = (wvlStart,wvlEnd)
+    apertureRadius=.5#arcsec
+    foldPlBins = functools.partial(foldPl,nPhaseBins=nPhaseBins,wvlRange=wvlRange,apertureRadius=apertureRadius)
+
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count()-1)
+    outDicts = pool.map(foldPlBins,plPaths)
+
     phaseProfiles = np.array([d['phaseProfile'] for d in outDicts])
     profileErrors = np.array([d['profileErrors'] for d in outDicts])
     phaseBinEdges = outDicts[0]['phaseBinEdges']
-    savePath = '/Scratch/dataProcessing/J0337/profiles2014.npz'
+    savePath = '/Scratch/dataProcessing/J0337/profiles2014_{}bins_{}-{}angstroms_{}arcsecAperture.npz'.format(nPhaseBins,wvlStart,wvlEnd,apertureRadius)
     np.savez(savePath,phaseProfiles=phaseProfiles,profileErrors=profileErrors,phaseBinEdges=phaseBinEdges,obsTimestamps=obsTimestamps)
     
     fig,ax = plt.subplots()
