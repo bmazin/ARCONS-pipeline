feadd8eb57d960c9bf1376bcddad3e87b08002c0
Megan Kelley <mkelley@turk.physics.ucsb.edu>
2015-10-12 14:17:45 -0700

    Megan's updates to J0002 image stack script

--- examples/Pal2014_1SWASP_J0002/makeImageStacks.py
+++ examples/Pal2014_1SWASP_J0002/makeImageStacks.py
@@ -19,31 +19,36 @@ path = '/Scratch/DisplayStack/PAL2014/1SWASP_J0002'
 verbose=True
 showPlot=True
 
-startWl = 9000
-endWl = 10000
+startWl = 4000
+endWl = 5000
 hp=True
 flat = True
+integrationTime=10
 
-
-outputFN = 'ImageStack_%s-%s'%(startWl, endWl)
+outputFN = '%s_%s-%s'%(integrationTime,startWl, endWl)
 if flat==True:
     outputFN+='_flat'
 if hp==True:
     outputFN+='_hp'
-outputFN+='.h5'
+identifier=outputFN
 
 print outputFN
 
+LC=LightCurve(fileID=identifier,path=path,targetName='1SWASP_J0002',run='PAL2014',verbose=True,showPlot=False)
+LC.makeImageStack(imageStackFilename='',dt=integrationTime,wvlStart=startWl,wvlStop=endWl,weighted=flat, fluxWeighted=False, getRawCount=False,scaleByEffInt=hp, deadTime=100.e-6)
+LC.makeAllCentroidFiles(centroidFilenames=['',''])
 
+'''
+#replace everything below here with lightCurve.py example under __main()__
 LC = LightCurve(path,verbose=verbose,showPlot=showPlot)
 obsFNs = LC.getFNs()
 #print 'obs[10]: ',obsFNs[10:11][0].obs()
 obsFiles = generateObsObjectList(obsFNs, wvlLowerLimit=startWl, wvlUpperLimit=endWl, loadHotPix=hp, loadWvlCal=True, loadFlatCal=flat, loadSpectralCal=False)
 print 'numObs: ',len(obsFiles)
-im_dict = getImages(fromObsFile=True, fromPhotonList=False, fromImageStack=False, obsFiles=obsFiles, integrationTime=10, weighted=flat,  fluxWeighted=False, getRawCount=False)
+im_dict = getImages(fromObsFile=True, fromPhotonList=False, fromImageStack=False, obsFiles=obsFiles, integrationTime=integrationTime, weighted=flat,  fluxWeighted=False, getRawCount=False)
 images=im_dict['images']
 pixIntTimes=im_dict['pixIntTimes']
 print 'numImages: ',len(images)
     
 writeImageStack(images=images, pixIntTimes=pixIntTimes, startTimes=im_dict['startTimes'], intTimes=im_dict['intTimes'], path=path, outputFilename=outputFN)
-
+'''
