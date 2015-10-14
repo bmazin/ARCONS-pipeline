52e6bdceaa56cace36684828568ac9382a6515c8
abwalter <abwalter@physics.ucsb.edu>
2015-10-14 10:37:11 -0700

    Minor updates to photometry code

--- photometry/PSF_Popup.py
+++ photometry/PSF_Popup.py
@@ -12,10 +12,9 @@ from photometry.plot3DImage import plot3DImage
 
 def clickCanvas(self,LC,event):
     if event.inaxes is self.axes and self.mpl_toolbar._active is None:
-        photometryDict = LC.getLightCurve(fromLightCurveFile=True)
-        time = photometryDict['startTimes']
+        time = LC.photometry_dict['startTimes']
         closest_time_ind = np.argmin(np.abs(time - event.xdata))
-        print event.xdata, ' --> ', time[closest_time_ind]
+        #print event.xdata, ' --> ', time[closest_time_ind]
         
         pop=PopUp(parent=self,title='JD: '+str(time[closest_time_ind])+' PSF fit')
         pop_image(pop,LC,closest_time_ind)
@@ -29,15 +28,15 @@ def clickCanvas(self,LC,event):
         pop_2DImage(pop,LC,closest_time_ind)
         pop.show()
         
+        print 'image:',closest_time_ind
+        #print 'centroid:',LC.centroids[closest_time_ind]
+        
 def pop_image(self,LC,ind,model='multiple_2d_circ_gauss_func'):
-    photometryDict = LC.getLightCurve(fromLightCurveFile=True)
-    im_dict = LC.getImages(fromImageStack=True)
-    
-    image = im_dict['images'][ind]
+    image = LC.im_dict['images'][ind]
     image[np.invert(np.isfinite(image))]=0.
     errs = np.sqrt(image)
     errs[np.where(image==0.)]=np.inf
-    parameters = photometryDict['parameters'][ind]
+    parameters = LC.photometry_dict['parameters'][ind]
     models = model_list[model](parameters)(p=np.ones(len(parameters)),data=image,return_models=True)
     guess = models[0]
     for m in models[1:]:
@@ -46,14 +45,11 @@ def pop_image(self,LC,ind,model='multiple_2d_circ_gauss_func'):
     plot3DImage(self.fig,self.axes,image,errs=errs,fit=guess)
     
 def pop_residualImage(self,LC,ind,model='multiple_2d_circ_gauss_func'):
-    photometryDict = LC.getLightCurve(fromLightCurveFile=True)
-    im_dict = LC.getImages(fromImageStack=True)
-    
-    image = im_dict['images'][ind]
+    image = LC.im_dict['images'][ind]
     image[np.invert(np.isfinite(image))]=0.
     errs = np.sqrt(image)
     errs[np.where(image==0.)]=np.inf
-    parameters = photometryDict['parameters'][ind]
+    parameters = LC.photometry_dict['parameters'][ind]
     models = model_list[model](parameters)(p=np.ones(len(parameters)),data=image,return_models=True)
     guess = models[0]
     for m in models[1:]:
@@ -64,13 +60,14 @@ def pop_residualImage(self,LC,ind,model='multiple_2d_circ_gauss_func'):
     residualImage[np.invert(np.isfinite(residualImage))]=0.
     self.plotArray(image=residualImage, title='Image Residual',cmap=matplotlib.cm.gnuplot2)
     
-def pop_2DImage(self,LC,ind):
-    im_dict = LC.getImages(fromImageStack=True)
-    
-    image = im_dict['images'][ind]
+def pop_2DImage(self,LC,ind):    
+    image = LC.im_dict['images'][ind]
     image[np.invert(np.isfinite(image))]=0.
-
     self.plotArray(image=image, title='Image',cmap=matplotlib.cm.gnuplot2)
+    
+    centroids = LC.centroids[ind]
+    for star_i in range(len(centroids)):
+        self.axes.plot(centroids[star_i][0],centroids[star_i][1],'gx')
     
 def hoverCanvas(self,time,event):
     if event.inaxes is self.axes:
@@ -78,20 +75,41 @@ def hoverCanvas(self,time,event):
         self.status_text.setText(str(time[closest_time_ind]))
 
 def plotLightCurve(self,LC):
-    photometryDict = LC.getLightCurve(fromLightCurveFile=True)
-    time = photometryDict['startTimes']
-    flags = photometryDict['flag']
-    flux=photometryDict['flux']
+    LC.loadLightCurve(photometryType='PSF')
+    time = LC.photometry_dict['startTimes']
+    flags = LC.photometry_dict['flag']
+    flux=LC.photometry_dict['flux']
     
     for star in range(len(flux[0])):
         lbl='target'
         if star>0: lbl='ref'+str(star-1)
         star_flux = flux[:,star]
-        self.axes.plot(time[np.where(flags==0.)],star_flux[np.where(flags==0.)],'.-',label=lbl)
+        self.axes.plot(time[np.where(flags==0.)],star_flux[np.where(flags==0.)],'.',label=lbl)
         self.axes.plot(time[np.where(flags==1.)],star_flux[np.where(flags==1.)],'r.')
         self.axes.plot(time[np.where(flags>1.1)],star_flux[np.where(flags>1.1)],'ro')
     self.axes.plot(time[np.where(flags==1.)],star_flux[np.where(flags==1.)],'r.',label='Centroid Fail')
     self.axes.plot(time[np.where(flags>1.1)],star_flux[np.where(flags>1.1)],'ro',label='Fit Fail')
+    self.axes.legend()
+    self.axes.set_xlabel('Julian Date')
+    self.axes.set_ylabel('Integrated Stellar Flux [counts/sec]')
+    
+    cid = self.fig.canvas.mpl_connect('button_press_event',partial(clickCanvas,self,LC))
+    cid = self.fig.canvas.mpl_connect('motion_notify_event', partial(hoverCanvas,self,time))
+    
+    self.fig.canvas.draw()
+    
+def plotLightCurveRatio(self,LC):
+    LC.loadLightCurve(photometryType='PSF')
+    time = LC.photometry_dict['startTimes']
+    flags = LC.photometry_dict['flag']
+    flux=LC.photometry_dict['flux']
+    
+    targetFlux=flux[:,0]
+    for star in range(len(flux[0])-1):
+        lbl=LC.targetName + '/Ref'+str(star)
+        ref_flux = flux[:,star+1]
+        self.axes.plot(time[np.where(flags==0.)],targetFlux[np.where(flags==0.)]/ref_flux[np.where(flags==0.)],'.',label=lbl)
+
     self.axes.legend()
     self.axes.set_xlabel('Julian Date')
     self.axes.set_ylabel('Integrated Stellar Flux [counts/sec]')
@@ -105,10 +123,22 @@ if __name__ == '__main__':
     #path = '/Scratch/DisplayStack/PAL2014/HAT_P1'
     #identifier = '1'
     path = '/Scratch/DisplayStack/PAL2014/1SWASP_J2210'
-    identifier = '0'
-    LC = LightCurve(path,fileID = identifier, PSF=True) 
+    identifier = '15s_4000-9000A_flat_hp_V3'
     
-    pop(plotFunc = partial(plotLightCurve,LC=LC),title="Light Curve Popup")
+    LC=LightCurve(fileID=identifier,path=path,targetName=None,run=None,verbose=True,showPlot=False) 
+    
+    pop(plotFunc = partial(plotLightCurve,LC=LC),title="PSF Light Curve Popup")
+    pop(plotFunc = partial(plotLightCurveRatio,LC=LC),title="PSF Light Curve Popup")
+
+
+
+    ##path = '/Scratch/DisplayStack/PAL2014/HAT_P1'
+    ##identifier = '1'
+    #path = '/Scratch/DisplayStack/PAL2014/1SWASP_J2210'
+    #identifier = '0'
+    #LC = LightCurve(path,fileID = identifier, PSF=True) 
+    #
+    #pop(plotFunc = partial(plotLightCurve,LC=LC),title="Light Curve Popup")
 
 
 
--- photometry/aper_Popup.py
+++ photometry/aper_Popup.py
@@ -9,8 +9,7 @@ from photometry.LightCurve import LightCurve
 
 def clickCanvas(self,LC,event):
     if event.inaxes is self.axes and self.mpl_toolbar._active is None:
-        photometryDict = LC.getLightCurve(photometryType='aperture',fromPhotometryFile=True,save=False)
-        time = photometryDict['startTimes']
+        time = LC.photometry_dict['startTimes']
         closest_time_ind = np.argmin(np.abs(time - event.xdata))
         #print event.xdata, ' --> ', time[closest_time_ind]
         
@@ -21,35 +20,54 @@ def clickCanvas(self,LC,event):
         pop=PopUp(parent=self,title='JD: '+str(time[closest_time_ind])+' Interpolated Image')
         pop_interpolatedImage(pop,LC,closest_time_ind)
         pop.show()
+        
+        print '\nimage:',closest_time_ind
+        #print 'centroid:',LC.centroids[closest_time_ind]
+        
 
 
         
 def pop_interpolatedImage(self,LC,ind):
-    #These should really be saved in the photometry file and grabbed from there...
-    interpolation = "linear"
-    aper_radius = 5
-    annulus_inner = 10
-    annulus_outer = 15
+    interpolation = LC.photometry_dict['interpolation'][ind]
+    aper_radius = LC.photometry_dict['apertureRad'][ind]
+    annulus_inner = LC.photometry_dict['annulusInnerRad'][ind]
+    annulus_outer = LC.photometry_dict['annulusOuterRad'][ind]
     
     image = LC.im_dict['images'][ind]
-    centroid = LC.centroids[ind]
+    centroids = LC.centroids[ind]
     interpImage = utils.interpolateImage(image, method=interpolation)
     angleArray = np.linspace(0,2*np.pi,180)
     
     self.plotArray(image=interpImage, title='Interpolated Image',cmap=matplotlib.cm.gnuplot2)
-    for star_i in range(len(centroid)):
-        self.axes.plot(centroid[star_i][0]+aper_radius*np.cos(angleArray), centroid[star_i][1] + aper_radius*np.sin(angleArray), 'b-')
-        self.axes.plot(centroid[star_i][0]+annulus_inner*np.cos(angleArray), centroid[star_i][1] + annulus_inner*np.sin(angleArray), 'r-')
-        self.axes.plot(centroid[star_i][0]+annulus_outer*np.cos(angleArray), centroid[star_i][1] + annulus_outer*np.sin(angleArray), 'r-')
+    for star_i in range(len(centroids)):
+        self.axes.plot(centroids[star_i][0]+aper_radius[star_i]*np.cos(angleArray), centroids[star_i][1] + aper_radius[star_i]*np.sin(angleArray), 'b-')
+        self.axes.plot(centroids[star_i][0]+annulus_inner[star_i]*np.cos(angleArray), centroids[star_i][1] + annulus_inner[star_i]*np.sin(angleArray), 'r-')
+        self.axes.plot(centroids[star_i][0]+annulus_outer[star_i]*np.cos(angleArray), centroids[star_i][1] + annulus_outer[star_i]*np.sin(angleArray), 'r-')
         
+        self.axes.plot(centroids[star_i][0],centroids[star_i][1],'gx')
     
     
         
 def pop_2DImage(self,LC,ind):
+
+    interpolation = LC.photometry_dict['interpolation'][ind]
+    aper_radius = LC.photometry_dict['apertureRad'][ind]
+    annulus_inner = LC.photometry_dict['annulusInnerRad'][ind]
+    annulus_outer = LC.photometry_dict['annulusOuterRad'][ind]
+    
     image = LC.im_dict['images'][ind]
     image[np.invert(np.isfinite(image))]=0.
-
+    centroids = LC.centroids[ind]
+    angleArray = np.linspace(0,2*np.pi,180)
+    
     self.plotArray(image=image, title='Image',cmap=matplotlib.cm.gnuplot2)
+    for star_i in range(len(centroids)):
+        self.axes.plot(centroids[star_i][0]+aper_radius[star_i]*np.cos(angleArray), centroids[star_i][1] + aper_radius[star_i]*np.sin(angleArray), 'b-')
+        self.axes.plot(centroids[star_i][0]+annulus_inner[star_i]*np.cos(angleArray), centroids[star_i][1] + annulus_inner[star_i]*np.sin(angleArray), 'r-')
+        self.axes.plot(centroids[star_i][0]+annulus_outer[star_i]*np.cos(angleArray), centroids[star_i][1] + annulus_outer[star_i]*np.sin(angleArray), 'r-')
+        
+        self.axes.plot(centroids[star_i][0],centroids[star_i][1],'gx')
+
 
 def hoverCanvas(self,time,event):
     if event.inaxes is self.axes:
@@ -57,10 +75,10 @@ def hoverCanvas(self,time,event):
         self.status_text.setText(str(time[closest_time_ind]))
 
 def plotLightCurve(self,LC):
-    photometryDict = LC.getLightCurve(photometryType='aperture',fromPhotometryFile=True,save=False)
-    time = photometryDict['startTimes']
-    flags = photometryDict['flag']
-    flux=photometryDict['flux']
+    LC.loadLightCurve(photometryType='aper')
+    time = LC.photometry_dict['startTimes']
+    flags = LC.photometry_dict['flag']
+    flux=LC.photometry_dict['flux']
     
     for star in range(len(flux[0])):
         lbl=LC.targetName
@@ -79,16 +97,39 @@ def plotLightCurve(self,LC):
     cid = self.fig.canvas.mpl_connect('motion_notify_event', partial(hoverCanvas,self,time))
     
     self.fig.canvas.draw()
+    
+def plotLightCurveRatio(self,LC):
+    LC.loadLightCurve(photometryType='aper')
+    time = LC.photometry_dict['startTimes']
+    flags = LC.photometry_dict['flag']
+    flux=LC.photometry_dict['flux']
+    
+    targetFlux=flux[:,0]
+    for star in range(len(flux[0])-1):
+        lbl=LC.targetName + '/Ref'+str(star)
+        ref_flux = flux[:,star+1]
+        self.axes.plot(time[np.where(flags==0.)],targetFlux[np.where(flags==0.)]/ref_flux[np.where(flags==0.)],'.',label=lbl)
+
+    self.axes.legend()
+    self.axes.set_xlabel('Julian Date')
+    self.axes.set_ylabel('Integrated Stellar Flux [counts/sec]')
+    
+    cid = self.fig.canvas.mpl_connect('button_press_event',partial(clickCanvas,self,LC))
+    cid = self.fig.canvas.mpl_connect('motion_notify_event', partial(hoverCanvas,self,time))
+    
+    self.fig.canvas.draw()
+    
 
 if __name__ == '__main__':
     #path = '/Scratch/DisplayStack/PAL2014/HAT_P1'
     #identifier = '1'
     path = '/Scratch/DisplayStack/PAL2014/1SWASP_J2210'
-    identifier = '0'
+    identifier = '15s_4000-9000A_flat_hp_V3'
     
     LC=LightCurve(fileID=identifier,path=path,targetName=None,run=None,verbose=True,showPlot=False) 
     
-    pop(plotFunc = partial(plotLightCurve,LC=LC),title="Light Curve Popup")
+    pop(plotFunc = partial(plotLightCurve,LC=LC),title="Aper Light Curve Popup")
+    pop(plotFunc = partial(plotLightCurveRatio,LC=LC),title="Aper Light Curve Popup")
 
 
 
